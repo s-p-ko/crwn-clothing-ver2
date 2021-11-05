72d4c4096abfe37a40878f320218d87772b9c369
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-11-05 23:52:38 +0200

    Formating the code

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,82 +1,82 @@
-import firebase from "firebase/compat/app"
-import "firebase/compat/firestore"
-import "firebase/compat/auth"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
-  apiKey: "AIzaSyCtcHO5YzCaSTbzznw1nuPwHfCBAmePHpM",
-  authDomain: "crwn-db-fc0e3.firebaseapp.com",
-  projectId: "crwn-db-fc0e3",
-  storageBucket: "crwn-db-fc0e3.appspot.com",
-  messagingSenderId: "6493568964",
-  appId: "1:6493568964:web:5b7b21ef0d14ba55302e91",
-  measurementId: "G-G7BQWBY9VB",
-}
+  apiKey: 'AIzaSyCtcHO5YzCaSTbzznw1nuPwHfCBAmePHpM',
+  authDomain: 'crwn-db-fc0e3.firebaseapp.com',
+  projectId: 'crwn-db-fc0e3',
+  storageBucket: 'crwn-db-fc0e3.appspot.com',
+  messagingSenderId: '6493568964',
+  appId: '1:6493568964:web:5b7b21ef0d14ba55302e91',
+  measurementId: 'G-G7BQWBY9VB',
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return
+  if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  const snapShot = await userRef.get()
+  const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
-    const { displayName, email } = userAuth
-    const createdAt = new Date()
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
         ...additionalData,
-      })
+      });
     } catch (error) {
-      console.log("error creating user", error.message)
+      console.log('error creating user', error.message);
     }
   }
 
-  return userRef
-}
+  return userRef;
+};
 
 export const addCollectionAndDocuments = async (
   collectionKey,
   objectsToAdd
 ) => {
-  const collectionRef = firestore.collection(collectionKey)
+  const collectionRef = firestore.collection(collectionKey);
 
-  const batch = firestore.batch()
+  const batch = firestore.batch();
   objectsToAdd.forEach((obj) => {
-    const newDocRef = collectionRef.doc()
-    batch.set(newDocRef, obj)
-  })
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
 
-  return await batch.commit()
-}
+  return await batch.commit();
+};
 
 export const convertCollectionsSnapshotToMap = (collections) => {
   const transformedCollection = collections.docs.map((doc) => {
-    const { title, items } = doc.data()
+    const { title, items } = doc.data();
 
     return {
       routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
       items,
-    }
-  })
+    };
+  });
 
   return transformedCollection.reduce((accumulator, collection) => {
-    accumulator[collection.title.toLowerCase()] = collection
-    return accumulator
-  }, {})
-}
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({ prompt: "select_account" })
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default firebase
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
-import { PersistGate } from "redux-persist/integration/react"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from "./redux/store"
+import { store, persistor } from './redux/store';
 
-import "./index.css"
-import App from "./App"
+import './index.css';
+import App from './App';
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
@@ -19,8 +19,8 @@ ReactDOM.render(
       </PersistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
-)
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
