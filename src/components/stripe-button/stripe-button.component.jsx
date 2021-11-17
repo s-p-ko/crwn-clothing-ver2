ed27a12a556e6b57c39dd922dae07a605f1cd30c
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-11-17 19:35:32 +0200

    Update package.json, yarn.lock, stripe button comonent

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51JnJG2HwTgE8g7huawhGmQstKDTGkgr9l2NDYtX2B02Z4XZbAXIczGg4WAZdj3zLh2KPrF0rStbtepfJnAAaIRjE00OsPbckuh';
+  const publishableKey = 'pk_test_bpk_test_51JnJG2HwTgE8g7huawhGmQstKDTGkgr9l2NDYtX2B02Z4XZbAXIczGg4WAZdj3zLh2KPrF0rStbtepfJnAAaIRjE00OsPbckuh7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
 
   const onToken = token => {
-    //console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
